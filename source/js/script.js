518a84aa21bcfa0ddbef166d4022482c21953bfc
PavlovaGalina <65108323+PavlovaGalina@users.noreply.github.com>
2021-01-03 22:19:04 +0300

    Автоматизируем сборку личного проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var pageHeaderOuterWrapper = document.querySelector('.page-header__outer-wpapper');
+var priceList = document.querySelector('.price__list');
+var toggleBase = document.querySelector('price__toggle--base');
+var toggleStandart = document.querySelector('price__toggle--standart');
+var toggleUnlim = document.querySelector('price__toggle--unlim');
 
 navMain.classList.remove('main-nav--nojs');
 pageHeaderOuterWrapper.classList.remove('page-header__outer-wpapper--nojs');
@@ -18,3 +22,15 @@ navToggle.addEventListener('click', function() {
     pageHeaderOuterWrapper.classList.remove('page-header__outer-wpapper--opened-menu');
   }
 });
+
+/**toggleBase.addEventListener('click', function() {
+  if (toggleStandart.classList.contains('slider__toggle--current') || toggleUnlim.classList.contains('slider__toggle--current')) {
+    if (toggleStandart.classList.contains('slider__toggle--current')) {
+      toggleStandart.classList.remove('slider__toggle--current');
+    } else {
+      toggleUnlim.classList.remove('slider__toggle--current');
+    }
+    toggleBase.classList.add('slider__toggle--current');
+    priceList.classList.add('price__list--base');
+  }
+});*/
